9221eb2264a408ee49fcfc329d21c84ee8ba6c96
shun.tomoyose <shun.tomoyose@g.softbank.co.jp>
2018-09-21 09:43:47 +0000

    ユーザ新規登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     unless @user
-      redirect_to new_task_path
+      redirect_to tasks_path
     end
   end
 
@@ -19,6 +19,20 @@ class UsersController < ApplicationController
 
   def create
     # ユーザ登録のpostを受けとってDBに新規レコードを追加
-
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザ登録完了'
+      redirect_to new_session_path
+    else
+      flash.now[:danger] = 'ユーザ登録失敗'
+      render :new
+    end
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
